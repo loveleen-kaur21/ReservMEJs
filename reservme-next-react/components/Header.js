65523e8cfe4f8e089bdb41e585262a55d32f0f16
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-13 10:24:42 -0500

    Created header/layout components

---
+++ reservme-next-react/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>ReservME</h1>
+            <p>Where you reserve for a ME!</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ reservme-next-react/components/Layout.js
@@ -0,0 +1,17 @@
+import Header from './Header'
+import styles from '../styles/Layout.module.css'
+
+const Layout = ({children}) => {
+    return (
+        <>
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <Header />
+                {children}
+            </main>
+        </div>
+        </>
+    )
+}
+
+export default Layout
