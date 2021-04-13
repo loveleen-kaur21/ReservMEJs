90cc1a852b48ef9adb4df2cb7274e1f487b94db4
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-13 14:56:16 -0500

    Buttn :)

--- reservme-next-react/components/RoomForms.js
+++ reservme-next-react/components/RoomForms.js
@@ -1,3 +1,4 @@
+import ToggleButton from '../components/ToggleButton'
 import styles from '../styles/Rooms.module.css'
 
 const RoomForms = ({rooms}) => {
@@ -5,18 +6,23 @@ const RoomForms = ({rooms}) => {
         <div>
             <div className={styles.roomCard}>
                 <h2>Studio</h2>
+                <ToggleButton />
             </div>
             <div className={styles.roomCard}>
                 <h2>1 Bedroom</h2>
+                <ToggleButton />
             </div>
             <div className={styles.roomCard}>
                 <h2>2 Bedroom</h2>
+                <ToggleButton />
             </div>
             <div className={styles.roomCard}>
                 <h2>Premium Suite</h2>
+                <ToggleButton />
             </div>
             <div className={styles.roomCard}>
                 <h2>Presidential</h2>
+                <ToggleButton />
             </div>
         </div>
     )
---
+++ reservme-next-react/components/ToggleButton.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Button.module.css'
+
+const ToggleButton = () => {
+    return (
+        <button className={styles.btn}>Show more</button>
+    )
+}
+
+export default ToggleButton
