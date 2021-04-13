efcffc97587ae2182600b0400fc3006e1e4835a6
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-13 16:09:53 -0500

    Added buttons to the rest of the rooms

--- reservme-next-react/components/RoomForms.js
+++ reservme-next-react/components/RoomForms.js
@@ -1,5 +1,9 @@
 import ToggleButton from '../components/ToggleButton'
 import StudioDesc from '../components/StudioDesc'
+import OneBedDesc from '../components/OneBedDesc'
+import TwoBedDesc from '../components/TwoBedDesc'
+import PremiumDesc from '../components/PremiumDesc'
+import PresidentialDesc from '../components/PresidentialDesc'
 import { useState, useEffect } from 'react'
 import styles from '../styles/Rooms.module.css'
 
@@ -19,19 +23,23 @@ const RoomForms = ({rooms}) => {
             </div>
             <div className={styles.roomCard}>
                 <h2>1 Bedroom</h2>
-                {/* <ToggleButton onClick={toggleButton} /> */}
+                <ToggleButton onClick={() => setShowOneBed(!showOneBedDesc)} btnColor={showOneBedDesc ? "pink": "blue"} text={showOneBedDesc ? "Hide" : "Show more"} textColor={showOneBedDesc ? "black" : "white"}/>
+                {showOneBedDesc && <OneBedDesc />}
             </div>
             <div className={styles.roomCard}>
                 <h2>2 Bedroom</h2>
-                {/* <ToggleButton onClick={toggleButton} /> */}
+                <ToggleButton onClick={() => setShowTwoBed(!showTwoBedDesc)} btnColor={showTwoBedDesc ? "pink": "blue"} text={showTwoBedDesc ? "Hide" : "Show more"} textColor={showTwoBedDesc ? "black" : "white"} />
+                {showTwoBedDesc && <TwoBedDesc />}
             </div>
             <div className={styles.roomCard}>
                 <h2>Premium Suite</h2>
-                {/* <ToggleButton onClick={toggleButton} /> */}
+                <ToggleButton onClick={() => setShowPremium(!showPremiumDesc)} btnColor={showPremiumDesc ? "pink": "blue"} text={showPremiumDesc ? "Hide" : "Show more"} textColor={showPremiumDesc ? "black" : "white"} />
+                {showPremiumDesc && <PremiumDesc />}
             </div>
             <div className={styles.roomCard}>
                 <h2>Presidential</h2>
-                {/* <ToggleButton onClick={toggleButton} /> */}
+                <ToggleButton onClick={() => setShowPresidential(!showPresidentialDesc)} btnColor={showPresidentialDesc ? "pink": "blue"} text={showPresidentialDesc ? "Hide" : "Show more"} textColor={showPresidentialDesc ? "black" : "white"} />
+                {showPresidentialDesc && <PresidentialDesc />}
             </div>
         </div>
     )
